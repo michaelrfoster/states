d446cdb67d11fa92f69ae6ffb26d4411a2bfcb82
CalvinBertonciniWM <clbertoncini@email.wm.edu>
2020-07-12 16:10:17 -0400

    started using geolocation data

--- App.js
+++ App.js
@@ -7,6 +7,21 @@
  */
 
 import React from 'react';
+//import Styled from 'styled-components/native';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import Geolocation from 'react-native-geolocation-service';
+//const Container = Styled.View '
+//flex: 1;'
+//;
+
+
+
+//const GoogleMap = () => {
+//return (<Container>
+//<MapView style={{flex: 1}} provider = {PROVIDER_GOOGLE} />
+//<Container>);
+//};
+
 import {
   SafeAreaView,
   StyleSheet,
@@ -14,6 +29,7 @@ import {
   View,
   Text,
   StatusBar,
+  PermissionsAndroid
 } from 'react-native';
 
 import {
@@ -24,51 +40,123 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+const my_styles = StyleSheet.create({
+map: {
+...StyleSheet.absoluteFillObject,
+},
+});
+
+function get_location ()
+{
+//if (hasLocationPermission)
+//{
+Geolocation.getCurrentPosition(
+    (position) => {
+    console.log(position);
+    },
+    (error) => {
+        console.log(error.code, error.message);
+    },
+    {enableHighAccuracy: true, timeout: 15000, maximumAge: 1000}
+    );
+//    }
+
+}
+
+async function request_location_permission() {
+try {
+    const granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+    {
+    title: "Give location permission",
+    message: "Let us know your location, so we can best help you!",
+    buttonNegative: "Cancel",
+    buttonPositive: "OK"
+    }
+    );
+    if (granted === PermissionsAndroid.RESULTS.GRANTED)
+    {
+    console.log("granted");
+
+        console.log("we are here");
+        get_location();
+    }
+    else
+    {
+    console.log("denied");
+    }
+    }
+    catch (err) { console.warn(err); }
+
+
+
+};
+
+    request_location_permission();
+
+
+    console.log("tag9999999994444444");
+    get_location();
+
+
 const App: () => React$Node = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+
+
+
+  //  <>
+
+    <MapView
+    style={my_styles.map}
+      initialRegion = {{
+      latitude: 37.78825,
+      longitude: -122.4324,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+      }} />
+
+
+  //    <StatusBar barStyle="dark-content" />
+  //    <SafeAreaView>
+  //      <ScrollView
+  //        contentInsetAdjustmentBehavior="automatic"
+  //        style={styles.scrollView}>
+  //        <Header />
+  //        {global.HermesInternal == null ? null : (
+//            <View style={styles.engine}>
+//              <Text style={styles.footer}>Engine: Hermes</Text>
+//            </View>
+//          )}
+//          <View style={styles.body}>
+//            <View style={styles.sectionContainer}>
+//              <Text style={styles.sectionTitle}>Step One</Text>
+//              <Text style={styles.sectionDescription}>
+//                Edit <Text style={styles.highlight}>App.js</Text> to change this
+//                screen and then come back to see your edits. Hi I did something! lol
+//              </Text>
+//            </View>
+//            <View style={styles.sectionContainer}>
+//              <Text style={styles.sectionTitle}>See Your Changes</Text>
+//              <Text style={styles.sectionDescription}>
+//                <ReloadInstructions />
+//              </Text>
+//            </View>
+//            <View style={styles.sectionContainer}>
+//              <Text style={styles.sectionTitle}>Debug</Text>
+//              <Text style={styles.sectionDescription}>
+//                <DebugInstructions />
+//              </Text>
+//            </View>
+//            <View style={styles.sectionContainer}>
+//              <Text style={styles.sectionTitle}>Learn More</Text>
+//              <Text style={styles.sectionDescription}>
+//                Read the docs to discover what to do next:
+//              </Text>
+//            </View>
+//            <LearnMoreLinks />
+//          </View>
+//        </ScrollView>
+//      </SafeAreaView>
+//   </>
   );
 };
 
